98322e4f83b203da92f7f2009c422a71d80fcf7f
Mohammed Sobhi <m.mohamed.sobhi.98@gmail.com>
2021-12-18 16:00:29 +0200

    UI/finish not found error page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import LandingPage from "./pages/LandingPage";
 import SearchResults from "./pages/SearchResults";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="/" element={<LandingPage />}></Route>
           <Route path="/search" element={<SearchResults />}></Route>
         </Routes>
